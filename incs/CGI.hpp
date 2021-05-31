a3dbb369b17279d79506f8122e1434353ee64be7
thervieu <thibault99hervieu@gmail.com>
2021-05-31 16:45:12 +0200

    CGI does not work, just pushing my progress

--- incs/CGI.hpp
+++ incs/CGI.hpp
@@ -4,4 +4,21 @@
 # include "Config.hpp"
 # include "Request.hpp"
 
+class CGI
+{
+	private:
+
+		std::string _full_path;
+		std::string _ressource_path;
+		location _loc;
+
+	public:
+
+		CGI(std::string, std::string, location);
+		~CGI(void);
+		std::string executeCGI(void);
+		std::string getArgs(void);
+
+};
+
 #endif
---
+++ srcs/CGI.cpp
@@ -0,0 +1,72 @@
+#include "CGI.hpp"
+
+// ressource should be in _loc ?
+CGI::CGI(std::string path, std::string ressource_path, location loc)
+{
+	_full_path = path;
+	_ressource_path = ressource_path;
+	_loc = loc;
+}
+
+//if need to del sthg
+CGI::~CGI(void)
+{
+}
+
+std::string executeCGI(void)
+{
+	int _pipe[2];
+	pid_t pid;
+	int status;
+
+	//get args into a char **
+
+	if (pipe(_pipe) != 0)
+	{
+		std::cout << "pipe failed" << std::endl;
+		exit(1);
+	}
+	
+	pid = fork();
+
+	if (pid == -1)
+	{
+		std::cout << "fork failed" << std::endl;
+		exit(1);
+	}
+	if (pid == 0)
+	{
+		close (_pipe[1]);
+		dup2(_pipe[0], 0);
+
+		int _file_fd;
+
+		_file_fd = open("/tmp/cgi_file", O_RDWR | O_CREAT | O_TRUNC | O_IRUSR | O_IWUSR)
+		if (_file_fd == -1)
+		{
+			std::cout << "open failed" << std::endl;
+			exit(1);
+		}
+		dup2(_file_fd, 1);
+		dup2(_file_fd, 2);
+		//int res = 0
+		//res = execve(_full_path, exec_and_args, args);
+		if (res == -1)
+		{
+			std::cout << "execve failed" << std::endl;
+			exit(1);
+		}
+		close (_file_fd);
+		close (_pipe[0]);
+		// free args
+		exit(1);
+	}
+	else
+	{
+		close(_pipe[0]);
+		write(_pipe[1], content, content.length());
+		close(_pipe[1]);
+		//free args
+	}
+	return (readFile("tmp/cgi_file"));
+}
