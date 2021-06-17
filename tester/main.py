0e0d031eb8068bd5eab42ddb4d374333a5bb0c9a
thervieu <thibault99hervieu@gmail.com>
2021-06-17 12:56:27 +0200

    tester added, doesn't work properly yet, pushed to keep trace but the main idea is here

---
+++ tester/main.py
@@ -0,0 +1,37 @@
+from tests import *
+from typing import Callable
+
+import os
+import sys
+
+def check_test(port: int, name_test: str, test: Callable) -> None:
+	try:
+		str_result = name_test(port)
+	except:
+		str_result = "couldn't connect"
+
+	if (len(str_result) == 0):
+		check = ✅
+	else:
+		check = ❌
+	
+	print("test : {} | result : {} {}".format(name_test, check, str_result))
+
+
+def run(port: int) -> None:
+	"""run tests"""
+
+
+
+
+if (__name__ == "__main__"):
+	if (len(sys.argv) != 2):
+		print("Usage: python3 main.py tests.py [port]")
+		exit(1)
+	try:
+		port = int(sys.argv[1])
+	except:
+		print("please input a valid port (int)")
+		exit(1)
+	os.system("rm -rf ./documents/tmp/*")
+	run(port)
