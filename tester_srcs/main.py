276c4f56d5ed41375e301ad4e5b28ef39717fa8d
thervieu <thibault99hervieu@gmail.com>
2021-06-26 17:51:36 +0200

    explaining tests

--- tester_srcs/main.py
+++ tester_srcs/main.py
@@ -20,21 +20,21 @@ def check_test(port: int, name_test: str, test: Callable) -> None:
 	else:
 		check = RED + "X" + DEFAULT
 	
-	print("test : {:35} | result : {} {}".format(name_test, check, str_result))
+	print("test : {:50} | result : {} {}".format(name_test, check, str_result))
 
 
 def run(port: int) -> None:
 	"""run tests"""
 	check_test(port, "GET / ", simple_get_index)
-	check_test(port, "GET /auto ", get_autoindex_subdir)
-	check_test(port, "GET /forbidden ", get_forbidden_dir)
+	check_test(port, "GET /auto autoindex", get_autoindex_subdir)
+	check_test(port, "GET /forbidden (403) ", get_forbidden_dir)
 	check_test(port, "GET non_existing_dir (404) ", get_404)
 	check_test(port, "GET / ports 8080 and 8081", get_index_two_ports)
 	check_test(port, "GET / 1 worker 50 times", fifty_get_root)
-	check_test(port, "POST / (method not authorized) ", wrong_method)
+	check_test(port, "POST / method not authorized (405)  ", wrong_method)
 	check_test(port, "POST /post ", simple_post)
-	check_test(port, "POST /post request too big", post_too_big)
-	check_test(port, "POST /post_upload", post_with_upload)
+	check_test(port, "POST /post request too big (413)", post_too_big)
+	check_test(port, "POST /post_upload -> upload in /upload", post_with_upload)
 	check_test(port, "DELETE /delete_folder/index.html", delete)
 
 	#check_test(port, "GET / 10 workers 100 times", stress_test)
