fc834becabe4fa98711bd64ca1c82a1aa2335e26
thervieu <thibault99hervieu@gmail.com>
2021-06-30 14:26:08 +0200

    Revert "saved two lines in GETresponse :')"
    This reverts commit c12c12372355258eaa6ad6d77ba10f6336ec7037.

--- srcs/Response.cpp
+++ srcs/Response.cpp
@@ -825,8 +825,10 @@ std::vector<char>	Response::GETResponse(void)
 			response += "Content-Length: " + ss.str() + "\r\n";
 		}
 		response += this->getContentLanguage() + "\r\n";
-		response += this->getLastModified() + "\r\n\r\n";
+		response += this->getLastModified() + "\r\n";
 		f_response.assign(response.begin(), response.end());
+		f_response.push_back('\r');
+		f_response.push_back('\n');
 		if (this->_request.getMethod().compare("GET") == 0 || this->_request.getMethod().compare("POST") == 0)
 			std::copy(file_content.begin(), file_content.end(), std::back_inserter<std::vector<char> >(f_response));
 	}
